7eea1c7a76d3035bb07f92abe0557172ccc944d2
Bartek <bborowin@gmail.com>
2015-12-19 00:53:42 -0500

    timeline concept 1

--- main.ctrl.js
+++ main.ctrl.js
@@ -6,7 +6,7 @@ function DisplayCtrl(dataService, canvasService) {
 	var params = {
 		margin: {top: 30, right: 20, bottom: 30, left: 50},
 		width: 1200,
-		height: 370
+		height: 2000
 	};
 
 	var c = canvasService.createCanvas('body', params);
@@ -15,29 +15,40 @@ function DisplayCtrl(dataService, canvasService) {
 	var svg = c.svg;
 
 	var items = dataService.roundDates(data);
-	var created = d3.nest().key(function(d){return d.created}).sortKeys(d3.ascending).entries(items);
-	created = _.map(created, function(c) { return {date: parseDate(c.key), count: c.values.length}});
+	_.forEach(items, function(item, index) {
+        item.date = parseDate(item.created);
+        if (item.resolved) {
+            item.end = parseDate(item.resolved);
+        } else {
+            item.end = item.date;
+        }
+		item.index = index;
+	});
 
-	var valueline = d3.svg.line()
-	    .x(function(d) { return c.scales.x(d.date); })
-	    .y(function(d) { return c.scales.y(d.count); });
+    var m = 15;
+	c.scales.x.domain(d3.extent(items, function(d) { return d.date; }));
+	c.scales.y.domain(d3.extent(items, function(d) { return m * d.index; }));
 
-	c.scales.x.domain(d3.extent(created, function(d) { return d.date; }));
-	c.scales.y.domain(d3.extent(created, function(d) { return d.count; }));
-
-	svg.append("path")
-        .attr("class", "line")
-        .attr("stroke", "blue")
-        .attr("d", valueline(created));
-
-    // Add the X Axis
     svg.append("g")
         .attr("class", "x axis")
         .attr("transform", "translate(0," + c.height + ")")
         .call(c.axes.x);
 
-    // Add the Y Axis
     svg.append("g")
         .attr("class", "y axis")
         .call(c.axes.y);
+
+    foo = svg.selectAll('line')
+       .data(items).enter()
+       .append('rect')
+       .attr('x', function(d) { return c.scales.x(d.date); })
+       .attr('width', function(d) { return c.scales.x(d.end) - c.scales.x(d.date); })
+       .attr('y', function(d) { return c.scales.y(m * d.index); })
+       .attr('height', function(d) { return c.scales.y(m * d.index) - c.scales.y(m * (d.index+0.35)); })
+       // .attr('x1', function(d) { return c.scales.x(d.date); })
+       // .attr('x2', function(d) { return c.scales.x(d.end); })
+       // .attr('y1', function(d) { return c.scales.y(m * d.index); })
+       // .attr('y2', function(d) { return c.scales.y(m * d.index); })
+       .attr('stroke', '#555555')
+       .attr('stroke-width', 1);
 }
